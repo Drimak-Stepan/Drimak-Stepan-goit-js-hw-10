5d74bafbb4de43454328ab2aa53129c653f638ec
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2022-11-30 23:13:57 +0200

    berorend

---
+++ src/fetchCountries.js
@@ -0,0 +1,10 @@
+export default function fetchCountries(name) {
+  return fetch(
+    `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(resp => {
+    if (!resp.ok) {
+      throw new Error(onFetchError);
+    }
+    return resp.json();
+  });
+}
