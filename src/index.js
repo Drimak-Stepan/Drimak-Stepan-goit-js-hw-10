41fe0cbf5f6eb0e414125c2e7b39e198956e779a
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2022-11-30 23:11:51 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,14 +1,7 @@
-getItemTemplate = ({ altSpellings, flags }) => `
-  <li class="country-list__item">
-    <svg class="" width="" height="">
-      <use href=${flags.svg}></use>
-    </svg>
-    <h2>${altSpellings[0]}</h2>
-  </li>`;
-
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
+import fetchCountries from './fetchCountries';
 
 const DEBOUNCE_DELAY = 3000;
 
@@ -18,23 +11,58 @@ const refs = {
   info: document.querySelector('.country-info'),
 };
 
-let items = [];
-
-const render = () => {
-  const list = items.map(item => getItemTemplate(item));
-  console.log(getItemTemplate());
-};
 const onSearch = e => {
   const { value } = e.target;
+  if (!value) {
+    refs.list.innerHTML = '';
+    refs.info.innerHTML = '';
+    return;
+  }
+  fetchCountries(value.trim()).then(render).catch(onFetchError);
+};
 
-  fetch(
-    `https://restcountries.com/v3.1/name/${value}?fields=name.official,capital,population,flags,languages`
-  )
-    .then(resp => resp.json())
-    .then(date => {
-      items = date;
-      render();
-    });
+const render = countries => {
+  let markup = '';
+
+  if (countries.length > 10) {
+    return Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.',
+      { clickToClose: true }
+    );
+  } else if (countries.length >= 2 && countries.length <= 10) {
+    markup = countries
+      .map(({ name, flags }) => {
+        return `<li class="country-list__item">
+        <img src="${flags.svg}" alt="${name.official}" width="50"/>
+        ${name.official}</li>`;
+      })
+      .join('');
+
+    refs.info.innerHTML = '';
+  } else {
+    markup = countries
+      .map(({ name, flags, capital, population, languages }) => {
+        return `<img src="${flags.svg}" alt="${name.official}" width="200"/>
+          <h2>${name.official}</h2>
+        <ul>
+          <li class="country-info__item">Capital:   ${capital}</li>
+          <li class="country-info__item">Population:   ${population}</li>
+          <li class="country-info__item">Languages:   ${Object.values(
+            languages
+          )}</</li>
+        </ul>`;
+      })
+      .join('');
+    refs.list.innerHTML = '';
+    return (refs.info.innerHTML = markup);
+  }
+  refs.list.innerHTML = markup;
 };
 
 refs.input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onFetchError() {
+  Notiflix.Notify.failure('Oops, there is no country with that name', {
+    clickToClose: true,
+  });
+}
