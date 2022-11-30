862a531907c8645ff5181f0eff1418e0aa7eafb3
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2022-11-30 10:38:03 +0200

    start10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
 
-const DEBOUNCE_DELAY = 300;
+const DEBOUNCE_DELAY = 3000;
+
+const refs = {
+  input: document.querySelector('#search-box'),
+  list: document.querySelector('.country-list'),
+  info: document.querySelector('.country-info'),
+};
+
+const onSearch = e => {
+  const { value } = e.target;
+
+  fetch(
+    `https://restcountries.com/v3.1/name/${value}?fields=name.official,capital,population,flags.svg,languages`
+  )
+    .then(resp => resp.json())
+    .then(date => {
+      console.log(date);
+    });
+};
+
+refs.input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
