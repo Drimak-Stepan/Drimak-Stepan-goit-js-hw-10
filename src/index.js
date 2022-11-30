8f3d4eb8f629e8749acc76620e81d66952c87d83
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2022-11-30 11:37:49 +0200

    s

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+getItemTemplate = ({ altSpellings, flags }) => `
+  <li class="country-list__item">
+    <svg class="" width="" height="">
+      <use href=${flags.svg}></use>
+    </svg>
+    <h2>${altSpellings[0]}</h2>
+  </li>`;
+
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 3000;
 
@@ -9,15 +18,22 @@ const refs = {
   info: document.querySelector('.country-info'),
 };
 
+let items = [];
+
+const render = () => {
+  const list = items.map(item => getItemTemplate(item));
+  console.log(getItemTemplate());
+};
 const onSearch = e => {
   const { value } = e.target;
 
   fetch(
-    `https://restcountries.com/v3.1/name/${value}?fields=name.official,capital,population,flags.svg,languages`
+    `https://restcountries.com/v3.1/name/${value}?fields=name.official,capital,population,flags,languages`
   )
     .then(resp => resp.json())
     .then(date => {
-      console.log(date);
+      items = date;
+      render();
     });
 };
 
