fccabb23417c9423ed3133fe29572f8fcc3e0a65
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2022-12-01 20:27:42 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 import fetchCountries from './fetchCountries';
 
-const DEBOUNCE_DELAY = 3000;
+const DEBOUNCE_DELAY = 300;
 
 const refs = {
   input: document.querySelector('#search-box'),
